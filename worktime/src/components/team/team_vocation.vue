6cf9a38768a501f5b3e756584c70f2c4edf4ff5c
xiaohuiliang <xiaohui.liang@ihandysoft.com>
2018-05-31 16:52:32 +0800

    teamvocation--搜索姓名

--- worktime/src/components/team/team_vocation.vue
+++ worktime/src/components/team/team_vocation.vue
@@ -1,12 +1,42 @@
 <template lang="html">
     <div class="">
-        team_vocation
+        <el-row :gutter="10">
+            <el-col :span="4">
+                <div class="">
+                    <el-input
+                        placeholder="请输入姓名"
+                        suffix-icon="el-icon-search"
+                        size="small"
+                        v-model="userName"
+                        @change="handleNameChange">
+                    </el-input>
+                </div>
+            </el-col>
+            <el-col :span="17">
+                <div class="">
+                </div>
+            </el-col>
+            <el-col :span="3">
+                <div class="">
+                </div>
+            </el-col>
+        </el-row>
     </div>
 
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            userName: '',
+        }
+    },
+    methods: {
+        handleNameChange() {
+            console.log("搜索姓名");
+        }
+    }
 }
 </script>
 
